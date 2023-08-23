99e9a8b8856d37de08d1f99a3ac26e6f11dc3a36
WangHao <1046911101@qq.com>
2023-08-23 15:44:02 +0800

    20230823

---
+++ Script/CLion/0321/CMakeBuild/CMakeFiles/test1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/WangHao/Sites/学习/CCpp/Script/CLion/0321/1/main.c" "CMakeFiles/test1.dir/1/main.c.o" "gcc" "CMakeFiles/test1.dir/1/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
