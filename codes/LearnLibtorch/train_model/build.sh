c3c2083e2ca32e5a922811f1c245b742d223aad7
WangHao <1046911101@qq.com>
2023-11-13 16:36:55 +0800

    [Normal]: 20231113

--- codes/LearnLibtorch/train_model/build.sh
+++ codes/LearnLibtorch/train_model/build.sh
@@ -1,4 +1,4 @@
-mkdir CMakeBuild
-cd CMakeBuild
+mkdir build
+cd build
 cmake -D CMAKE_PREFIX_PATH=/opt/homebrew/opt/pytorch/share/cmake ..
 cmake --build . --config Release
--- timestamps/build.sh
+++ timestamps/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义要创建和使用的构建目录
-BUILD_DIR="CMakeBuild"
+BUILD_DIR="build"
 
 # 检查目录是否存在，如果存在，则删除
 if [ -d "$BUILD_DIR" ]; then
