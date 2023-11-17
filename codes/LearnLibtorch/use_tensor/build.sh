7b65d696f836c1999f50753a05e098a4e6e92a84
WangHao <1046911101@qq.com>
2023-11-17 18:07:18 +0800

    [Normal]: 20231117

--- codes/LearnLibtorch/use_tensor/build.sh
+++ codes/LearnLibtorch/use_tensor/build.sh
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake -D CMAKE_PREFIX_PATH=/opt/homebrew/opt/pytorch/share/cmake ..
+cmake -DCMAKE_PREFIX_PATH=/opt/homebrew/opt/pytorch/share/cmake ..
 cmake --build . --config Release  # 具有跨平台一致性，不用管底层的构建系统（如 Make、Ninja、MSBuild 等）
